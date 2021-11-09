0849a7f89cebfca8b85e8aa4b392cc6b93a41fe6
Mikey <mikeybess27@gmail.com>
2021-11-09 14:42:11 -0600

    Instantiate/Invoke method
    Also made the template arrays public

--- Assets/Scripts/RoomSpawner.cs
+++ Assets/Scripts/RoomSpawner.cs
@@ -5,28 +5,51 @@ using UnityEngine;
 public class RoomSpawner : MonoBehaviour
 {
     [SerializeField] int _openingDir;
-    // 1 = Down Door
-    // 2 = Up Door
-    // 3 = Left Door
-    // 4 = Right Door
+    // 1 = Up Room (Down Door)
+    // 2 = Down Roomn (Up Door)
+    // 3 = Right Room (Left Door)
+    // 4 = Left Room (Right Door)
 
-    private void Update()
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+
+    private void Start()
     {
-        if(_openingDir == 1)
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.5f);
+    }
+
+    private void Spawn()
+    {
+        if (spawned == false)
         {
-            // Spawn a room UP
-        }
-        else if(_openingDir == 2)
-        {
-            // Spawn a room DOWN
-        }
-        else if (_openingDir == 3)
-        {
-            // Spawn a room to the LEFT
-        }
-        else if (_openingDir == 4)
-        {
-            // Spawn a room to the RIGHT
+            if (_openingDir == 1)
+            {
+                // Spawn a room UP
+                rand = Random.Range(0, templates._upRooms_D.Length);
+                Instantiate(templates._upRooms_D[rand], transform.position, Quaternion.identity);
+            }
+            else if (_openingDir == 2)
+            {
+                // Spawn a room DOWN
+                rand = Random.Range(0, templates._downRooms_U.Length);
+                Instantiate(templates._downRooms_U[rand], transform.position, Quaternion.identity);
+            }
+            else if (_openingDir == 3)
+            {
+                // Spawn a room to the LEFT
+                rand = Random.Range(0, templates._leftRooms_R.Length);
+                Instantiate(templates._leftRooms_R[rand], transform.position, Quaternion.identity);
+            }
+            else if (_openingDir == 4)
+            {
+                // Spawn a room to the RIGHT
+                rand = Random.Range(0, templates._rightRooms_L.Length);
+                Instantiate(templates._rightRooms_L[rand], transform.position, Quaternion.identity);
+            }
+
+            spawned = true;
         }
     }
 
--- Assets/Scripts/RoomTemplates.cs
+++ Assets/Scripts/RoomTemplates.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class RoomTemplates : MonoBehaviour
 {
-    [SerializeField] private GameObject[] _upRooms_D;
-    [SerializeField] private GameObject[] _downRooms_U;
-    [SerializeField] private GameObject[] _leftRooms_R;
-    [SerializeField] private GameObject[] _rightRooms_L;
+    [SerializeField] public GameObject[] _upRooms_D;
+    [SerializeField] public GameObject[] _downRooms_U;
+    [SerializeField] public GameObject[] _leftRooms_R;
+    [SerializeField] public GameObject[] _rightRooms_L;
 }
