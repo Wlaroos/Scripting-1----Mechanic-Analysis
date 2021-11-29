383e6ecd64cec826a677c8a217fa217584f2bb5c
Mikey <mikeybess27@gmail.com>
2021-11-29 17:06:17 -0600

    Minimap camera movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform _ref;
+
+    void Update()
+    {
+        transform.position = new Vector3(((_ref.position.x / 416) * 10), ((_ref.position.y / 208) * 10 ), -1);
+    }
+}
