49d5c5d14098cc1166d2220aeb5c3232cec4d468
Mikey <mikeybess27@gmail.com>
2021-11-30 13:00:45 -0600

    Minimap Fog of War

---
+++ Assets/MapFog.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapFog : MonoBehaviour
+{
+    [SerializeField] GameObject _ref;
+    Color def;
+    bool ful = false;
+
+    private void Start()
+    {
+        _ref = GameObject.Find("Map Icon");
+        DelayHelper.DelayAction(this, DelayedColor, .02f);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "Main")
+        {
+            Full();
+            ful = true;
+        }
+        else if (ful == false)
+        {
+            Black();
+        }
+    }
+
+    void DelayedColor()
+    {
+        def = GetComponent<SpriteRenderer>().color;
+        GetComponent<SpriteRenderer>().color = new Color(def.r, def.g, def.b, 0);
+    }
+
+    void Black()
+    {
+        GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 1);
+    }
+
+    void Full()
+    {
+        GetComponent<SpriteRenderer>().color = new Color(def.r, def.g, def.b, 1);
+    }
+}
